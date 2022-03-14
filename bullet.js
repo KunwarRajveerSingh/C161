2aadec0429b139e0b826efccef7f6af0eb474d38
KunwarRajveerSingh <73644014+KunwarRajveerSingh@users.noreply.github.com>
2022-03-14 18:49:29 +0530

    Add files via upload

---
+++ bullet.js
@@ -0,0 +1,30 @@
+AFRAME.registerComponent('bullet', {
+    init: function(){
+        this.shootbullets();
+    },
+    shootbullets: function(){
+        window.addEventListener("keydown", e => {
+            if(e.key === 'z'){
+                var bullet = document.createElement('a-entity');
+                bullet.setAttribute('geometry', {
+                    primitive: 'sphere',
+                    radius: 0.1
+                });
+                bullet.setAttribute('material', {
+                    color: 'black'
+                });
+                var cam = document.querySelector('#camera');
+                var p = cam.getAttribute("position")
+                bullet.setAttribute('position', p);     
+                var camera = document.querySelector('#camera').object3D;
+                var direction = new THREE.Vector3();
+                camera.getWorldDirection(direction);
+                bullet.setAttribute('velocity',direction.multiplyScalar(-10));
+                var scene = document.querySelector('#scene');
+
+                scene.appendChild(bullet);
+                //cam.appendChild(bullet);
+            }
+        })
+    }
+})
